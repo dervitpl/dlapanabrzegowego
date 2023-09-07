8a0e4f31351620341324751876c9fd9489a7d3f9
dervitpl <kacpermucha420@gmail.com>
2023-09-07 14:49:17 +0200

    edycja nareszcie smiga

--- ManageMe/src/app/components/projects/projects.component.ts
+++ ManageMe/src/app/components/projects/projects.component.ts
@@ -9,20 +9,21 @@ import { ProjectService } from '../../services/project.service';
 })
 export class ProjectsComponent implements OnInit {
   projects: Project[] = [];
-
-  constructor(private projectService: ProjectService) {}
-
-  ngOnInit(): void {
-    this.projects = this.projectService.getProjects();
-  }
+  editingProjectID: number | null = null;
+  editingProject: Project = { id: 0, name: '', description: '', owner: '' };
   newProject: Project = {
     id: 0,
     name: '',
     description: '',
     owner: 'Jan Kowalski'
   };
-  
-  
+
+  constructor(private projectService: ProjectService) {}
+
+  ngOnInit(): void {
+    this.projects = this.projectService.getProjects();
+  }
+
   addProject(): void {
     if (this.newProject.name.trim()) {
       this.newProject.id = Date.now();
@@ -32,32 +33,32 @@ export class ProjectsComponent implements OnInit {
       this.loadProjects();
     }
   }
-  
+
   loadProjects(): void {
     this.projects = this.projectService.getProjects();
   }
-  deleteProject(project: Project): void {
-    const index = this.projects.indexOf(project);
+
+  deleteProject(projectId: number) {
+    const index = this.projects.findIndex(p => p.id === projectId);
     if (index > -1) {
-        this.projects.splice(index, 1);
+      this.projects.splice(index, 1);
     }
-}
-  isEditing: boolean = false;
-  editingProject: Project = { id: 0, name: '', description: '', owner: '' };
-
-  editProject(project: Project): void {
-    this.isEditing = true;
-    this.editingProject = { ...project }; // kopiujemy projekt, aby nie edytować bezpośrednio oryginalnego obiektu
-}
-saveProject(): void {
-  const index = this.projects.findIndex(p => p.id === this.editingProject.id);
-  if (index > -1) {
-      this.projects[index] = this.editingProject;
   }
-  this.isEditing = false;
-}
 
+  startEditing(project: Project) {
+    this.editingProjectID = project.id;
+    this.editingProject = { ...project };
+  }
 
+  saveEdit() {
+    const index = this.projects.findIndex(p => p.id === this.editingProjectID);
+    if (index !== -1) {
+      this.projects[index] = this.editingProject;
+      this.editingProjectID = null;
+    }
+  }
 
-  
+  cancelEdit() {
+    this.editingProjectID = null;
+  }
 }
