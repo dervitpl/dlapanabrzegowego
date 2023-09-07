d030ce6115aabe4fbd7e22a67c260f16c84a3641
dervitpl <kacpermucha420@gmail.com>
2023-09-07 12:23:51 +0200

    init

--- ManageMe/src/app/app-routing.module.ts
+++ ManageMe/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { FunctionalitiesComponent } from './components/functionalities/functionalities.component';
+import { TasksComponent } from './components/tasks/tasks.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/projects', pathMatch: 'full' },
+  { path: 'projects', component: ProjectsComponent },
+  { path: 'projects/:projectId/functionalities', component: FunctionalitiesComponent },
+  { path: 'functionalities/:functionalityId/tasks', component: TasksComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- ManageMe/src/app/app.module.ts
+++ ManageMe/src/app/app.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ProjectsComponent } from './projects/projects.component';
-import { FunctionalitiesComponent } from './functionalities/functionalities.component';
-import { TasksComponent } from './tasks/tasks.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { FunctionalitiesComponent } from './components/functionalities/functionalities.component';
+import { TasksComponent } from './components/tasks/tasks.component';
 
 @NgModule({
   declarations: [
